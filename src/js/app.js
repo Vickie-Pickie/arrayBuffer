fca7ac404ee916da7667cccf76d449f5eca8e43b
viktoriamastrenko <victoriamastrenko@gmail.com>
2022-02-20 14:15:09 +0800

    fixed lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 export function getBuffer(data) {
   const buffer = new ArrayBuffer(data.length * 2);
   const bufferView = new Uint16Array(buffer);
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i += 1) {
     bufferView[i] = data.charCodeAt(i);
   }
   return buffer;
